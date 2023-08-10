1330c747bd546b00128c98a79a6d3c17c5ecf512
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-10 13:54:43 +0000

    10 Ağu 2023 16:54

--- flutter_ml_model_deployment/lib/ml_model.dart
+++ flutter_ml_model_deployment/lib/ml_model.dart
@@ -9,10 +9,12 @@ class _MlModel extends State<MlModel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         backgroundColor: Colors.pink,
         title: Text('ML model deployment with Flutter',),
       ),
+      body: Center(child: Text('ML with Flutter')),
     );
   }
 }
